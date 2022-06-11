dabcc7a30de1d499ea1425a609101968afefda3f
Rob Young <rhyoung214@gmail.com>
2022-06-10 19:37:16 -0500

    Sorted section bidhistory and evals

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,8 +1,15 @@
 class SectionsController < ApplicationController
   before_action :set_course, only: %i[index show]
   before_action :set_section, only: :show
+
+  QUARTER_SORT = %w(Summer Spring Winter Autumn)
+  ACADEMIC_YEAR_SORT = %w(2021-2022 2020-2021 2019-2020 2018-2019 2017-2018 2016-2017)
+
   def index
     @all_sections = Section.where(course_id: @course.id)
+                           .in_order_of(:academic_year, ACADEMIC_YEAR_SORT)
+                           .in_order_of(:quarter, QUARTER_SORT)
+
     @sections = @all_sections.where(academic_year: '2021-2022', course_id: @course.id)
   end
 
